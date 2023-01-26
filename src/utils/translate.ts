7a008b5b7684da8cc5f66535b711233e9c2f6078
Cyliann <maks.cych@wp.pl>
2023-01-26 20:51:30 +0100

    Add reverse and forward translation

--- src/utils/translate.ts
+++ src/utils/translate.ts
@@ -9,19 +9,7 @@ interface CodonsDictType {
   };
 }
 
-const translate = (sequence: string, direction: InputOutputProps['directions'], method: InputOutputProps['method']) => {
-  const newSequence = sequence
-    // remove line breaks
-    .replace(/(\r\n|\n|\r)/gm, '')
-    // split into array
-    .split('')
-    // convert to uppercase
-    .map(char => char.toUpperCase())
-    // replace thymine with uracil
-    .map(val => (val === 'T' ? 'U' : val))
-    // join back into string
-    .join('');
-
+const convertToAminoAcids = newSequence => {
   // join every 3 codon positions for 3 variants
   const codons: { [key: string]: string[][] } = {};
   for (let i = 0; i < 3; i += 1) {
@@ -39,6 +27,32 @@ const translate = (sequence: string, direction: InputOutputProps['directions'], 
   Object.values(codons).forEach(codon =>
     aminoAcids.push(codon.map(value => (codonsDict as CodonsDictType)?.[value[0]]?.[value[1]]?.[value[2]]).join('')),
   );
+
+  return aminoAcids;
+};
+
+const translate = (sequence: string, direction: InputOutputProps['directions'], method: InputOutputProps['method']) => {
+  const newSequence = sequence
+    // remove line breaks
+    .replace(/(\r\n|\n|\r)/gm, '')
+    // split into array
+    .split('')
+    // convert to uppercase
+    .map(char => char.toUpperCase())
+    // replace thymine with uracil
+    .map(val => (val === 'T' ? 'U' : val))
+    // join back into string
+    .join('');
+
+  let aminoAcids: string[] = [];
+
+  if (direction.forward) {
+    aminoAcids = aminoAcids.concat(convertToAminoAcids(newSequence));
+  }
+  if (direction.reverse) {
+    // run translation on reversed sequence
+    aminoAcids = aminoAcids.concat(convertToAminoAcids(newSequence.split('').reverse().join('')));
+  }
 
   return aminoAcids;
 };
