98193eafb622f0187e891872ee5e00393b1746e6
Maciej Kurzak <kmk.kurzak@gmail.com>
2023-03-05 16:39:54 +0100

    displaying images

---
+++ src/components/ImageViewer.tsx
@@ -0,0 +1,29 @@
+import { Box, Image, Text } from '@chakra-ui/react';
+import React, { useEffect } from 'react';
+import getPeptideImage from '../utils/images';
+
+function ImageViewer({ sequence }) {
+  const [image, setImage] = React.useState([]);
+
+  useEffect(() => {
+    if (!sequence) return;
+    setImage(getPeptideImage(sequence));
+  }, [sequence]);
+
+  const [loading, setLoading] = React.useState(true);
+
+  return (
+    <Box height="16rem" overflowX="hidden" display="flex" justifyContent="center" alignItems="center">
+      {loading ? (
+        <Text textAlign="center" fontSize="xl" fontWeight="bold" color="gray.500">
+          Loading image...
+        </Text>
+      ) : null}
+      <Box height="100%" overflowX="auto">
+        <Image height="100%" maxWidth="unset" src={image} onLoad={() => setLoading(false)} alt="" />
+      </Box>
+    </Box>
+  );
+}
+
+export default ImageViewer;
