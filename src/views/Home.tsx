c67ab0baa58e8ff1cb5a27249ffbdb9e5173bde0
Maciej Kurzak <kmk.kurzak@gmail.com>
2023-03-05 16:59:12 +0100

    add loading on home page

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Card, CardBody, Code, FormControl, FormLabel, Heading, HStack, LinkOverlay, Stack, Switch } from '@chakra-ui/react';
+import { Box, Card, CardBody, Code, FormControl, FormLabel, Heading, HStack, LinkOverlay, Stack, Switch } from '@chakra-ui/react';
 import DrawerComponent from '../components/Drawer';
 
-function Home({ drawerProps, inputProps, resultProteins }) {
+function Home({ drawerProps, inputProps, resultProteins, isLoading }) {
   const [protsState, setProtsState] = React.useState(true);
   const [nonProtsState, setNonProtsState] = React.useState(true);
 
@@ -45,30 +45,38 @@ function Home({ drawerProps, inputProps, resultProteins }) {
               />
             </FormControl>
           </HStack>
-          {resultProteins.map(protein =>
-            (protein.pdbId && protsState) || (!protein.pdbId && nonProtsState) ? (
-              <Card
-                direction={{ base: 'column', sm: 'row' }}
-                variant="filled"
-                key={protein.index}
-                transition="all 0.2s"
-                _hover={{
-                  transform: 'translateY(-3px)',
-                }}
-              >
-                <Stack>
-                  <CardBody>
-                    <LinkOverlay href={`/result?sequence=${protein.sequence}${protein.pdbId ? `&pdbid=${protein.pdbId}` : ''}`}>
-                      <Heading size="md">{protein.pdbId ? `Protein ${protein.pdbId}` : 'Sequence'}</Heading>
-                    </LinkOverlay>
+          {isLoading ? (
+            <Box display="flex" flexGrow="1" flexDirection="column" justifyContent="center" alignItems="center">
+              <Heading size="lg" color="gray.500">
+                Loading...
+              </Heading>
+            </Box>
+          ) : (
+            resultProteins.map(protein =>
+              (protein.pdbId && protsState) || (!protein.pdbId && nonProtsState) ? (
+                <Card
+                  direction={{ base: 'column', sm: 'row' }}
+                  variant="filled"
+                  key={protein.index}
+                  transition="all 0.2s"
+                  _hover={{
+                    transform: 'translateY(-3px)',
+                  }}
+                >
+                  <Stack>
+                    <CardBody>
+                      <LinkOverlay href={`/result?sequence=${protein.sequence}${protein.pdbId ? `&pdbid=${protein.pdbId}` : ''}`}>
+                        <Heading size="md">{protein.pdbId ? `Protein ${protein.pdbId}` : 'Sequence'}</Heading>
+                      </LinkOverlay>
 
-                    <Code marginTop={5} wordBreak="break-all">
-                      {protein.sequence}
-                    </Code>
-                  </CardBody>
-                </Stack>
-              </Card>
-            ) : null,
+                      <Code marginTop={5} wordBreak="break-all">
+                        {protein.sequence}
+                      </Code>
+                    </CardBody>
+                  </Stack>
+                </Card>
+              ) : null,
+            )
           )}
         </Stack>
       </Stack>
